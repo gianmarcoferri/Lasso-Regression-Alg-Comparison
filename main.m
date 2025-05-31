1cfa0dec097bde50271bbeea5a0a8a69b83f1a2f
Gian Marco Ferri <142916941+gianmarcoferri@users.noreply.github.com>
2025-05-31 16:56:02 +0200

    Update main.m

--- main.m
+++ main.m
@@ -1,33 +1,30 @@
-% Lasso implementation
+% California Housing Lasso Regression Comparison
 
+% Read dataset
+dataset = readtable('california_housing_processed.csv');
 
+% Normalize features (optional but typical for regression)
+dataset{:, 1:8} = normalize(dataset{:, 1:8}, "range"); % Columns 1-8 are features
 
-% import data
-dataset = readtable('dataset.csv');
-% normalizing data between [0,1] 
-dataset{:, [1 3 4]} = normalize(dataset{:, [1 3 4]}, "range");
-
-
-% data split(train: 80%, test: 20%) -> randomized!
+% Split data: 80% train, 20% test
 cv = cvpartition(size(dataset,1),'HoldOut',0.2);
 idx = cv.test;
-% % Separate to training and test data
 train = dataset(~idx,:);
 test  = dataset(idx,:);
 
-X = train{:, 1:9};
-Y = train{:, 10};
-X_test = test{:, 1:9};
-Y_test = test{:, 10};
+X = train{:, 1:8}; % features: longitude ... median_income
+Y = train{:, 9};   % target: median_house_value
+X_test = test{:, 1:8};
+Y_test = test{:, 9};
 
-
-% Parameters
+% Hyperparameters
 iterations = 50000; 
 step_size = 0.01;
 l1_penalty = 1;
 tolerance = 1e-4;
+agents = 8;
 
-% Soft-thresholding Lasso 
+% ISTA (Gradient Descent)
 disp("GD");
 lasso = LassoReg(step_size, iterations, l1_penalty, tolerance);
 f1 = @() lasso.fit(X, Y, "gd");
@@ -36,12 +33,10 @@ disp(t_gd);
 disp(lasso.iterations);
 Y_predicted = lasso.predict(X_test);
 disp(corrcoef(Y_test, Y_predicted).^2);     % R2
-% predict plot
 plot_predict("Lasso GD", Y_test, Y_predicted);
 plot_loss(lasso, "Loss GD");
 
-
-% % ADMM Lasso
+% ADMM
 disp("ADMM");
 lasso_admm = LassoReg(step_size, iterations, l1_penalty, tolerance);
 f2 = @() lasso_admm.fit(X, Y, "admm");
@@ -50,25 +45,20 @@ disp(t_admm);
 disp(lasso_admm.iterations);
 Y_predicted = lasso_admm.predict(X_test);
 disp(corrcoef(Y_test, Y_predicted).^2);     % R2
-% plot
 plot_predict("Lasso ADMM", Y_test, Y_predicted);
 plot_loss(lasso_admm, "Convergence ADMM");
 
-
-% Distributed Lasso
+% Distributed ADMM
 disp("Distributed ADMM");
-agents = 9;
 lasso_dist = LassoReg(step_size, iterations, l1_penalty, tolerance);
 f3 = @() lasso_dist.fit(X, Y, "dist", agents);
-t_dist = timeit(f3)
+t_dist = timeit(f3);
 disp(t_dist/agents);
 disp(lasso_dist.iterations);
 Y_predicted = lasso_dist.predict(X_test);
 disp(corrcoef(Y_test, Y_predicted).^2);     % R2
-% plot
 plot_predict("Lasso Distributed-ADMM", Y_test, Y_predicted);
 plot_loss(lasso_dist,  "Convergence Distributed-ADMM");
-
 
 function plot_predict(label, Y_test, Y_predicted)
     figure
@@ -76,8 +66,8 @@ function plot_predict(label, Y_test, Y_predicted)
     title(label);
     scatter(Y_test,Y_predicted)
     plot(Y_test,Y_test)
-    xlabel('Actual label')
-    ylabel('Predicted label')
+    xlabel('Actual value')
+    ylabel('Predicted value')
     hold off
 end
 
