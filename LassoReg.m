0becd80a398e86dafbca15d54b412befc2d51136
Gian Marco Ferri <142916941+gianmarcoferri@users.noreply.github.com>
2025-05-31 16:55:40 +0200

    Update LassoReg.m

--- LassoReg.m
+++ LassoReg.m
@@ -1,7 +1,4 @@
 classdef LassoReg < handle
-    %LASSOREGRESSION Summary of this class goes here
-    %   Detailed explanation goes here
-    
     properties
         step_size
         max_iterations
@@ -17,9 +14,7 @@ classdef LassoReg < handle
     end
     
     methods
-        function obj = LassoReg(step_size,max_iterations, l1_penalty, tolerance)
-            %LASSOREGRESSION Construct an instance of this class
-            %   Detailed explanation goes here
+        function obj = LassoReg(step_size, max_iterations, l1_penalty, tolerance)
             obj.step_size = step_size;
             obj.max_iterations = max_iterations;
             obj.l1_penalty = l1_penalty;
@@ -27,27 +22,20 @@ classdef LassoReg < handle
         end
         
         function fit(obj, X, Y, algo, agents)
-            obj.m = size(X, 1); % samples
-            obj.n = size(X, 2); % features
+            obj.m = size(X, 1); % number of samples
+            obj.n = size(X, 2); % number of features
             
             obj.W = zeros(1, obj.n);
             obj.X = X;
             obj.Y = Y;
            
-
             if algo == "gd"
-                % gradient descent
                 obj.gradient_descent();
             elseif algo == "admm"
-                % ADMM
                 obj.admm();
-            else 
-                % Distributed ADMM
+            else % "dist"
                 obj.distributed_admm(agents);
             end
-%             disp(algo);
-%             disp(obj.iterations);
-%             disp(obj.W);
         end
         
         function admm(obj)
@@ -66,55 +54,22 @@ classdef LassoReg < handle
                 z = obj.soft_threshold(obj.W + u, obj.l1_penalty/rho);
                 u = u + obj.W - z;
                 
-                r_norm  = norm(obj.W - z);          % primary residual
-                s_norm  = norm(-rho*(z - last_z));  % dual residual
-                tol_prim = sqrt(obj.n)*abs_tol + rel_tol*max(norm(obj.W), norm(-z));    % primary tolerance
-                tol_dual= sqrt(obj.n)*abs_tol + rel_tol*norm(rho*u);                    % dual tolerance
+                r_norm  = norm(obj.W - z);
+                s_norm  = norm(-rho*(z - last_z));
+                tol_prim = sqrt(obj.n)*abs_tol + rel_tol*max(norm(obj.W), norm(-z));
+                tol_dual= sqrt(obj.n)*abs_tol + rel_tol*norm(rho*u);
                 
                 obj.iterations = i;
                 obj.J(1,i) = r_norm;
                 obj.J(2,i) = s_norm;
                 obj.J(3,i) = tol_prim;
                 obj.J(4,i) = tol_dual;
-
-
-%                 obj.J(i) = obj.loss_function(obj.Y, obj.X*obj.W, z);
-
-%                 if obj.W - z == 0
-                if r_norm < tol_prim && s_norm < tol_dual   % stopping crit
+                
+                if r_norm < tol_prim && s_norm < tol_dual
                     break
                 end
             end
             obj.W = obj.W';
-%             disp(obj.J);
-            
-        end
-        
-        function gradient_descent(obj)
-            for i = 1:obj.max_iterations
-                Y_predict = obj.predict(obj.X);
-                
-                % gradients
-%                 dW = zeros(1, obj.n);
-%                 for j = 1 : obj.n
-%                     dW(j) =   -2 * obj.X(:,j)' * (obj.Y - Y_predict);
-%                     soft_term = obj.soft_threshold(obj.W(j), 1)
-%                     dW(j) = (dW(j) + soft_term) / obj.m;
-%                 end
-
-                soft_term = obj.soft_threshold(obj.W, obj.l1_penalty);
-                dW = (-2 * obj.X' * (obj.Y - Y_predict) + soft_term') / obj.m;
-                new_W = obj.W - obj.step_size * dW';     % update weights
-                
-                if mean(abs(new_W - obj.W)) < obj.tolerance   % stopping crit
-                    break
-                end   
-                
-                obj.J(i) = mean(abs(new_W - obj.W));
-                obj.W = new_W;
-                obj.iterations = i;
-%                 obj.J(i) = obj.loss_function(obj.Y, Y_predict, obj.W);
-            end
         end
         
         function distributed_admm(obj, agents)
@@ -126,7 +81,6 @@ classdef LassoReg < handle
             rel_tol = abs_tol * 100; 
             converged = 0;
             
-            % split by data
             [r,c] = size(obj.X);
             splitted_X   = permute(reshape(obj.X',[c,r/agents,agents]),[2,1,3]);
             splitted_Y = reshape(obj.Y,[r/agents,agents]);
@@ -136,63 +90,63 @@ classdef LassoReg < handle
             for i = 1:obj.max_iterations
                 last_z = z;
                 for j = 1:agents                    
-                    obj.W(j,:) = (permute(splitted_X(:,:,j), [2,1,3])*splitted_X(:,:,j) + rho*I)^(-1) * (permute(splitted_X(:,:,j), [2,1,3])*splitted_Y(:,j) + rho*(z-u(j,:))');
+                    obj.W(j,:) = (permute(splitted_X(:,:,j), [2,1,3])*splitted_X(:,:,j) + rho*I)^(-1) * ...
+                                 (permute(splitted_X(:,:,j), [2,1,3])*splitted_Y(:,j) + rho*(z-u(j,:))');
                     z = obj.soft_threshold(mean(obj.W) + mean(u), obj.l1_penalty/rho);
                     u(j,:) = u(j,:) + (obj.W(j,:) - z);
                     
-                    r_norm  = norm(mean(obj.W) - z);             % primary residual
-                    s_norm  = norm(-rho*(z - last_z));          % dual residual
-                    tol_prim = sqrt(obj.n)*abs_tol + rel_tol*max(norm(mean(obj.W)), norm(-z));      % primary tolerance
-                    tol_dual= sqrt(obj.n)*abs_tol + rel_tol*norm(rho*mean(u));                      % dual tolerance
+                    r_norm  = norm(mean(obj.W) - z);
+                    s_norm  = norm(-rho*(z - last_z));
+                    tol_prim = sqrt(obj.n)*abs_tol + rel_tol*max(norm(mean(obj.W)), norm(-z));
+                    tol_dual= sqrt(obj.n)*abs_tol + rel_tol*norm(rho*mean(u));
                     
-                    % loss for each agent
-%                     obj.J(j,i) = obj.loss_function(splitted_Y(:,j), splitted_X(:,:,j)*obj.W(j,:)', z);
                     obj.J(1,i,j) = r_norm;
                     obj.J(2,i,j) = s_norm;
                     obj.J(3,i,j) = tol_prim;
                     obj.J(4,i,j) = tol_dual;
-
-%                     if obj.W(j,:) - z == 0
-                    if r_norm < tol_prim && s_norm < tol_dual   % stopping crit
+                    
+                    if r_norm < tol_prim && s_norm < tol_dual
                         converged = 1;
                         obj.J = obj.J(:,:,j);
                         break
                     end
                 end
                 obj.iterations = i;
-
                 if converged
                     break
                 end
             end
-%             disp(obj.W);
-%             obj.J = mean(obj.J);
             obj.W = mean(obj.W);
         end
         
-        % H(x)
+        function gradient_descent(obj)
+            for i = 1:obj.max_iterations
+                Y_predict = obj.predict(obj.X);
+                
+                soft_term = obj.soft_threshold(obj.W, obj.l1_penalty);
+                dW = (-2 * obj.X' * (obj.Y - Y_predict) + soft_term') / obj.m;
+                new_W = obj.W - obj.step_size * dW';
+                
+                if mean(abs(new_W - obj.W)) < obj.tolerance
+                    break
+                end   
+                
+                obj.J(i) = mean(abs(new_W - obj.W));
+                obj.W = new_W;
+                obj.iterations = i;
+            end
+        end
+        
         function Y_predict = predict(obj, X)  
             Y_predict = X * obj.W';
         end
         
-        % loss function
         function loss = loss_function(obj, Y, Y_predict, W)
             loss = ( 1/2*sum((Y - Y_predict).^2) + obj.l1_penalty*norm(W,1) );
         end
       
-        % SOFT-THRESHOLD
         function soft_term = soft_threshold(~, w, th)
-%             if w > th
-%                 soft_term = w - th;
-%             elseif w < -th
-%                 soft_term = w + th;
-%             else
-%                 soft_term = 0;
-%             end
             soft_term = max(0, w-th) - max(0, -w-th);
         end
-        
- 
     end
 end
-
